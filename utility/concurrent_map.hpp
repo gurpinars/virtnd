bb1a065f0cd621e9c51ba0bc24639ce70cd858ed
M. Sami GÜRPINAR <sami.gurpinar@gmail.com>
2020-10-09 11:50:58 +0300

    Update concurrent_map.hpp

--- utility/concurrent_map.hpp
+++ utility/concurrent_map.hpp
@@ -1,51 +1,125 @@
+/**
+ * @file concurrent_map.hpp
+ * @author M. Sami GÜRPINAR <sami.gurpinar@gmail.com>
+ *
+ * @brief
+ * Implementation of concurrent_map.
+ */
+
 #pragma once
 
 #include <map>
 #include <mutex>
 
-
-template<typename _Key, typename _Val, typename const_iterator =typename decltype(std::map<_Key, _Val>{})::const_iterator>
+template<typename K, typename V>
 class concurrent_map {
 public:
-    _Val find(const _Key k) {
-        std::lock_guard<std::mutex> lockq{mtx};
-        auto found = container.find(k);
-        if (found != container.end())
-            return found->second;
-        return {};
-    }
+    using const_iterator = typename std::map<K, V>::const_iterator;
 
-    void insert(const _Key k, _Val &&v) {
-        std::lock_guard<std::mutex> lockq{mtx};
-        container.insert(std::make_pair(k, std::forward<_Val>(v)));
-    }
+    V find(const K &key);
 
-    void update(const _Key k, _Val &&v) {
-        std::lock_guard<std::mutex> lockq{mtx};
-        container[k] = std::forward<_Val>(v);
-    }
+    void insert(const K &key, V &&v);
 
-    void erase(const_iterator it) {
-        std::lock_guard<std::mutex> lockq{mtx};
-        container.erase(it);
-    }
+    void insert(const K &key, const V &v);
 
-    auto cbegin() const {
-        std::lock_guard<std::mutex> lockq{mtx};
-        return container.cbegin();
-    }
+    void update(const K &key, V &&v);
 
-    auto cend() const {
-        std::lock_guard<std::mutex> lockq{mtx};
-        return container.cend();
-    }
+    template<typename... Args>
+    void emplace(Args &&... args);
 
+    V &operator[](const K &k);
+
+    void erase(const_iterator it);
+
+    void erase(const K &key);
+
+    auto cbegin() const;
+
+    auto cend() const;
+
+    auto rbegin() const;
+
+    auto rend() const;
 
 private:
     mutable std::mutex mtx;
-    std::map<_Key, _Val> container;
+    std::map<K, V> container;
 
 };
 
+template<typename K, typename V>
+V concurrent_map<K, V>::find(const K &key) {
+    std::lock_guard<std::mutex> lockq{mtx};
+    auto found = container.find(key);
+    if (found != container.end())
+        return found->second;
+    return {};
+}
+
+template<typename K, typename V>
+void concurrent_map<K, V>::insert(const K &key, V &&v) {
+    emplace(key, std::move(v));
+
+}
+
+template<typename K, typename V>
+void concurrent_map<K, V>::insert(const K &key, const V &v) {
+    emplace(key, v);
+}
 
 
+template<typename K, typename V>
+void concurrent_map<K, V>::update(const K &key, V &&v) {
+    std::lock_guard<std::mutex> lockq{mtx};
+    container[key] = std::move(v);
+}
+
+template<typename K, typename V>
+template<typename... Args>
+void concurrent_map<K, V>::emplace(Args &&... args) {
+    std::lock_guard<std::mutex> lockq{mtx};
+    container.emplace(std::forward<Args>(args)...);
+
+}
+
+template<typename K, typename V>
+V &concurrent_map<K, V>::operator[](const K &k) {
+    std::lock_guard<std::mutex> lockq{mtx};
+    return container[k];
+}
+
+template<typename K, typename V>
+void concurrent_map<K, V>::erase(const_iterator it) {
+    std::lock_guard<std::mutex> lockq{mtx};
+    container.erase(it);
+}
+
+template<typename K, typename V>
+void concurrent_map<K, V>::erase(const K &key) {
+    std::lock_guard<std::mutex> lockq{mtx};
+    container.erase(key);
+}
+
+template<typename K, typename V>
+auto concurrent_map<K, V>::cbegin() const {
+    std::lock_guard<std::mutex> lockq{mtx};
+    return container.cbegin();
+}
+
+template<typename K, typename V>
+auto concurrent_map<K, V>::cend() const {
+    std::lock_guard<std::mutex> lockq{mtx};
+    return container.cend();
+}
+
+template<typename K, typename V>
+auto concurrent_map<K, V>::rbegin() const {
+    std::lock_guard<std::mutex> lockq{mtx};
+    return container.rbegin();
+}
+
+template<typename K, typename V>
+auto concurrent_map<K, V>::rend() const {
+    std::lock_guard<std::mutex> lockq{mtx};
+    return container.rend();
+}
